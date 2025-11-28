71e33311b2b90f2984e791ba7d88ae24969fdb18
Anandadonelly <anandadonelly@gmail.com>
2025-11-28 18:26:37 +0700

    Initial commit for loading screen

---
+++ android/app/src/main/kotlin/com/example/flutter_loading_screen/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_loading_screen
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ web/script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Simulasi loading selesai (opsional)
+    // Anda bisa hapus atau gunakan ini untuk pindah halaman setelah beberapa detik
+    // setTimeout(() => {
+    //     console.log('Loading selesai!');
+    //     // window.location.href = 'halaman-selanjutnya.html';
+    // }, 4000);
+});
