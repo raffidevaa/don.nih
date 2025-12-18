c4f1c9ab06589037a6f66a97b7381b5c4f123094
Bernadetta Graciela <grace.b1645@gmail.com>
2025-12-18 14:08:33 +0700

    fix:admin-homepage and admin-menu-control

--- lib/data/datasources/topping_datasource.dart
+++ lib/data/datasources/topping_datasource.dart
@@ -16,4 +16,37 @@ class ToppingDataSource {
         .map((e) => ToppingModel.fromMap(e))
         .toList();
   }
+
+  /// CREATE new topping
+  Future<ToppingModel> createTopping(String name, int price) async {
+    final response = await client
+        .from('toppings')
+        .insert({
+          'name': name,
+          'price': price,
+        })
+        .select()
+        .single();
+
+    return ToppingModel.fromMap(response);
+  }
+
+  /// UPDATE topping
+  Future<void> updateTopping(int id, String name, int price) async {
+    await client
+        .from('toppings')
+        .update({
+          'name': name,
+          'price': price,
+        })
+        .eq('id', id);
+  }
+
+  /// DELETE topping
+  Future<void> deleteTopping(int id) async {
+    await client
+        .from('toppings')
+        .delete()
+        .eq('id', id);
+  }
 }
